1a1c5f89ae7ba5531a6fddb6307a03d0d45ca6ea
soul00 <soul00555@gmail.com>
2019-04-28 14:57:53 +0300

    Learn hot

---
+++ JS Lessons/stringObjects.js
@@ -0,0 +1,20 @@
+console.log("Катюска".length);
+
+console.log("Another \"very long\" string");
+
+var string = "Sometimes the same is different";
+
+console.log('string.charAt(0):', string.charAt(string.length - 1));
+console.log('string.length:', string.length);
+console.log('string.substring(10):', string.substring(10));
+console.log('string.substring(10, 21):', string.substring(10, 21));
+
+console.log('string.slice(-10):', string.slice(-10));
+console.log('string.substr(14,4):', string.substr(14,4));
+console.log('string.indexOf("i"):', string.indexOf("same"));
+console.log('string.lastIndexOf("the"):', string.lastIndexOf("the"));
+console.log('string.replace("is", "is not");:', string.replace("is", "is not"));
+console.log('string.split(" "):', string.split(" "));
+console.log('string.toUpperCase:', string.toUpperCase());
+console.log('string.toLowerCase():', string.toLowerCase());
+
