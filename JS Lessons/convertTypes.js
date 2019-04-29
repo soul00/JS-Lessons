41ab7dd746978d2d33535417fe08a6936348c0ff
soul00 <soul00555@gmail.com>
2019-04-29 12:49:07 +0300

    Lear how to convert types of variables

---
+++ JS Lessons/convertTypes.js
@@ -0,0 +1,30 @@
+console.log(Number("666"));
+console.log(typeof Number("666"));
+
+console.log('String(4423):', String(4423));
+console.log(typeof String(4423));
+
+console.log('Boolean(1):', Boolean(1));
+console.log(typeof Boolean(1));
+
+console.log('!!5:', !!5);
+console.log('!!0:', !!0);
+
+console.log('2415 + "":', (2415 + ""));
+console.log('typeof 2415 + "":', typeof (2415 + ""));
+
+console.log('+"241":', (+"241"));
+console.log('typeof +"241":', typeof (+"241"));
+
+var number = 22;
+console.log('number.toString():', number.toString());
+console.log('typeof number.toString():', typeof number.toString());
+
+
+//Parsers
+
+console.log('parseInt("34 px", 10):', parseInt("34 px", 10));
+console.log('parseFloat("12.3", em):', parseFloat("12.35 em"));
+
+
+
